a5fb93b6c9507456be9176ed3cfb908ac91ab191
PaoloPascucci <pascuccip9@gmail.com>
2021-11-23 09:26:52 +0100

    create repo and link js

---
+++ script1.js
@@ -0,0 +1,35 @@
+// What is the largest number in the list?
+//   Example:
+// largest([7, 6, 5, 4, 3, 2, 1])
+
+
+// ==========
+// Functions
+// ==========
+function largest(numbers) {
+
+  function search(larg, arr) {
+    if (arr.length > 0) {
+      if (arr[0] > larg) {
+        return search(arr[0], arr.splice(0));
+      } else {
+        return search(larg, arr.splice(0));
+      }
+    } else {
+      return larg;
+    }
+  }
+
+  return search(0, numbers);
+}
+
+
+// ==========
+// Test
+// ==========
+
+console.log(largest([2, 6, 5, 9, 30, 2, 1]));
+console.log(largest([50, 6, 1, 2, 4, 2, 8]));
+console.log(largest([10, 8, 9, 10, 20, 8, 6]));
+console.log(largest([5, 16, 55, 4, 10, 21, 11]));
+
